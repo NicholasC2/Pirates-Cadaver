5f0465eadafc30c399fd81495d94700a0c0fc0a1
John Doe <john.doe@example.com>
2025-07-21 13:03:54 +1200

    Initial Commit

---
+++ src/client/java/com/nick/PiratesCadaverClient.java
@@ -0,0 +1,10 @@
+package com.nick;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PiratesCadaverClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
